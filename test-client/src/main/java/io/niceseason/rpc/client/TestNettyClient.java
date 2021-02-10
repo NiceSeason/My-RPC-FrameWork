0ad5c2ed3a355fbe8cba23f4c26a4bf9b442b0fb
NiceSeason <hongshengmo@163.com>
2021-02-10 22:32:20 +0800

    addReadme

--- test-client/src/main/java/io/niceseason/rpc/client/TestNettyClient.java
+++ test-client/src/main/java/io/niceseason/rpc/client/TestNettyClient.java
@@ -12,8 +12,6 @@ public class TestNettyClient {
         RpcClientProxy proxy = new RpcClientProxy(client);
         HelloService proxy1 = proxy.getProxy(HelloService.class);
         HelloObject object = new HelloObject(1, "sada");
-//        for (int i = 0; i < 10; i++) {
-            System.out.println(proxy1.hello(object));
-//        }
+        System.out.println(proxy1.hello(object));
     }
 }
